da0220370b71d3bf33a9997b0e30e1cff3980251
Alex Serra <alexserrarodriguez19@gmail.com>
2021-05-10 21:58:16 +0200

    Buttons creats

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -7,6 +7,7 @@ import { useDispatch } from "react-redux";
 import { IsLoadingHoc } from "../components/HOCS/IsLoadingHOC";
 import { HeaderBar } from "../components/shared/HeaderBar"
 import PhrasesService from "../services/phrasesService";
+import App from '../App';
 
 interface IHomeScreenProps {
     navigation: any;
@@ -21,17 +22,16 @@ const HomeScreen = (props: IHomeScreenProps) => {
 
 
     useEffect(() => {
-        props.setLoading(true)
         async function loadInitialPhrase() {
             await loadPhrase()
-            props.setLoading(false)
         }
-
         loadInitialPhrase()
     }, []);
 
 
     const loadPhrase = async () => {
+        props.setLoading(true)
+
         let response = await PhrasesService.getPhrase()
         let text
 
@@ -41,6 +41,13 @@ const HomeScreen = (props: IHomeScreenProps) => {
         else text = "Something went wrong!"
 
         setPhrase(text)
+        props.setLoading(false)
+
+    }
+
+    const savePhrase = () => {
+
+        console.log('inside save', phrase)
     }
 
 
@@ -51,6 +58,25 @@ const HomeScreen = (props: IHomeScreenProps) => {
             />
             <View style={styles.phraseContainer}>
                 <Text style={styles.phraseText}>{phrase}</Text>
+            </View>
+            <View style={{ flex: 1, flexDirection: "row", paddingHorizontal: normalize(10)}}>
+                <View style={{ flex: 1 }}>
+                    <TouchableOpacity
+                        onPress={() => loadPhrase()}
+                        style={styles.button1}
+                    >
+                        <Text style={styles.buttonText1}>Give Me Another</Text>
+                    </TouchableOpacity>
+
+                </View>
+                <View style={{ flex: 1 }}>
+                    <TouchableOpacity
+                        onPress={() => savePhrase()}
+                        style={styles.button2}
+                    >
+                        <Text style={styles.buttonText2}>Love It!</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         </SafeAreaView>
 
@@ -77,5 +103,31 @@ const styles = StyleSheet.create({
         fontSize: normalize(30),
         textAlign: "center",
         fontStyle: "italic"
+    },
+
+    button1: {
+        width: '100%',
+        height: normalize(50),
+        backgroundColor: AppConstants.colors.black,
+    },
+
+    button2: {
+        width: '100%',
+        height: normalize(50),
+        backgroundColor: AppConstants.colors.white,
+        borderColor: AppConstants.colors.black,
+        borderWidth: 2,
+    },
+
+    buttonText1: {
+        fontSize: normalize(20),
+        color: AppConstants.colors.white,
+        textAlign: "center"
+    },
+
+    buttonText2: {
+        fontSize: normalize(20),
+        color: AppConstants.colors.black,
+        textAlign: "center"
     }
 });
